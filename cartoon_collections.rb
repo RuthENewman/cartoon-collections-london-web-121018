2c6288afa763d4431d811a7cab8ddf9686951306
ruthnewman <ruthnewman@gmail.com>
2018-11-30 22:07:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+    names.each |index, name|
+    names[name] = index
+  end
+  names
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)
   # Your code here
+  planeteer_calls = []
+  counter = 0
+  calls.each do |call|
+    planeteer_calls << "#{call}!,"
+    counter += 1
+  end
+  planeteer_calls
 end
 
 def long_planeteer_calls# code an argument here
